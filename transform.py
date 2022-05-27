b7c50eb330d5b89c47c3145ea0f7e072a174499f
0ssamaak0 <0ssamaak0@gmail.com>
2022-05-27 20:26:34 +0200

    lane detection

---
+++ transform.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+
+def per_transform(img):  # first one
+    # Get the image x and y dimensions, will be used in the destination points
+    img_size = (img.shape[1], img.shape[0])
+    # Identify the source points
+    src = np.float32([[510, 460], [750, 460], [150, 650], [1200, 650]])
+    # identify the destination points
+    offset = 150
+    dst = np.float32([[offset, offset], [img_size[0]-offset, offset],
+                      [offset, img_size[1]-offset],
+                      [img_size[0]-offset, img_size[1]-offset]
+                      ])
+    # Get the transformation matrix necissary for mapping
+    M = cv2.getPerspectiveTransform(src, dst)
+    # Get the inverse matrix to transform the image back
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    # wrap the image
+    warped = cv2.warpPerspective(img, M, img_size)
+    return warped, M, Minv
+
+
+def transform(img, M):
+    img_size = (img.shape[1], img.shape[0])
+    warped = cv2.warpPerspective(img, M, img_size)
+    return warped
